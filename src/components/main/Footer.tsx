bf238da9a96e9d48e7beefd1220577ce7248b507
EG-L <pshowx12@naver.com>
2026-05-24 00:44:52 +0900

    contact 버튼 추가

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
 
 function Footer() {
+  const handleContact = () => {};
+
   return (
     <Fragment>
         <section id="contact">
@@ -8,14 +10,24 @@ function Footer() {
             <div className="ct-grid">
             <div className="ct-card ca">
                 <p className="ct-name">JI <br/>SANGWON</p>
-                <a href="#" className="ct-lnk">✉ sangwon@email.com</a>
-                <a href="#" className="ct-lnk">⌥ github.com/sangwon</a>
-                <a href="#" className="ct-lnk">in linkedin.com/in/sangwon</a>
+                <div className="ct-links">
+                    <a href="#" className="ct-lnk">✉ sangwon@email.com</a>
+                    <a href="#" className="ct-lnk">⌥ github.com/sangwon</a>
+                    <a href="#" className="ct-lnk">in linkedin.com/in/sangwon</a>
+                </div>
+                <div className="ct-btn-wrap">
+                    <button className="ct-btn ca" onClick={handleContact}>Contact</button>
+                </div>
             </div>
             <div className="ct-card cb">
                 <p className="ct-name">LEE <br/>EUIGWANG</p>
-                <a href="#" className="ct-lnk">✉ pshowx12@naver.com</a>
-                <a href="#" className="ct-lnk">⌥ github.com/EG-L</a>
+                <div className="ct-links">
+                    <a href="#" className="ct-lnk">✉ pshowx12@naver.com</a>
+                    <a href="#" className="ct-lnk">⌥ github.com/EG-L</a>
+                </div>
+                <div className="ct-btn-wrap">
+                    <button className="ct-btn cb" onClick={handleContact}>Contact</button>
+                </div>
             </div>
             </div>
             <p className="footer">© 2026 JI SANGWON × LEE EUIGWANG</p>

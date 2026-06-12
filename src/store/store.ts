1b6771aad60032fb4c65e37e204b1755d4b055e9
EG-L <pshowx12@naver.com>
2026-06-12 11:11:51 +0900

    console 로그 삭제

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { createLogger } from "redux-logger";
 import qnaReducer from "../slices/qnaSlice";
 import memberReducer from "../slices/memberSlice";
-
-const logger = createLogger();
 
 const store = configureStore({
     reducer: {
         qna: qnaReducer,
         member: memberReducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
     devTools: process.env.NODE_ENV !== 'production',
 });
 
